ed99f4d0c3593a3a8b967be4f079c3ce34b6a9ee
rcarbajsa <rcarbajso@gmail.com>
2019-06-14 17:18:08 +0200

    Me voy, estoy hasta los cojones de vosotros

--- jade/src/agents/Agent2.java
+++ jade/src/agents/Agent2.java
@@ -16,8 +16,36 @@ import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.UnreadableException;
 
 public class Agent2 extends Agent {
+	
+	private static final long serialVersionUID = 1L;
 	private ArrayList<String> userData;
 	private AID addressAgent3;
+	public class ReceiveMessageBehaviour extends OneShotBehaviour { 
+		private static final long serialVersionUID = 1L;
+		private final MessageTemplate mt = MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
+				MessageTemplate.MatchPerformative(ACLMessage.FAILURE));
+//		private Agent agent;
+		private Object obj;
+		public ReceiveMessageBehaviour(Agent agent) {
+			super(agent);
+		}
+		public void action() {
+			ACLMessage message = this.myAgent.blockingReceive(mt);
+			try {
+				this.obj = message.getContentObject();
+				System.out.println(this.myAgent.getName() + ": Ha llegado el mensaje");
+			} catch (UnreadableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		public Object getMessage() {
+			return this.obj;
+		}
+
+	}
+
 	class getAddress extends OneShotBehaviour{
 		private static final long serialVersionUID = 1L;
 		public void action() {
@@ -59,10 +87,9 @@ public class Agent2 extends Agent {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		ReceiveMessageBehaviour rcv = new ReceiveMessageBehaviour(this);
-		addBehaviour(rcv);
+		addBehaviour(new ReceiveMessageBehaviour(this));
 		addBehaviour(new getAddress());
-		addBehaviour(new SendMessageBehaviour(this, rcv.getMessage(), "REQUEST", addressAgent3));
+		addBehaviour(new SendMessageBehaviour(this,this.userData , "REQUEST", addressAgent3));
 	}
 
 }
--- jade/src/agents/Agent3.java
+++ jade/src/agents/Agent3.java
@@ -6,13 +6,37 @@ import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 public class Agent3 extends Agent {
-	
+	private Object obj;
+	public class ReceiveMessageBehaviour extends OneShotBehaviour { 
+		private static final long serialVersionUID = 1L;
+		private final MessageTemplate mt = MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
+				MessageTemplate.MatchPerformative(ACLMessage.FAILURE));
+		public ReceiveMessageBehaviour(Agent agent) {
+			super(agent);
+		}
+		public void action() {
+			ACLMessage message = this.myAgent.blockingReceive(mt);
+			try {
+				obj = message.getContentObject();
+				System.out.println(this.myAgent.getName() + ": Ha llegado el mensaje");
+			} catch (UnreadableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
 	public void setup() {
 		DFAgentDescription dfd = new DFAgentDescription();
 		dfd.setName(getAID());
 		ServiceDescription sd = new ServiceDescription();
+		//Cambiar nombre
 		sd.setName("Interfaz");
 		sd.setType("Interfaz");
 		dfd.addServices(sd);
@@ -20,9 +44,9 @@ public class Agent3 extends Agent {
 			DFService.register( this, dfd);
 			System.out.println("Agent3: Servicio publicado");
 		} catch (FIPAException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//Implementar Agente3
+		addBehaviour(new ReceiveMessageBehaviour(this));
+
 	}
 }
