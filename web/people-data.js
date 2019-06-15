115e221706a701d42ef4b2d5d51bae7de230d497
max <maximo.garcia.martinez@alumnos.upm.es>
2019-06-15 20:51:48 +0200

    more fixes

--- web/people-data.js
+++ web/people-data.js
@@ -1,57 +1,57 @@
 let peopleData = [{
     "score": 0.7,
     "sentences": [{
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "Positiva."
+    }, {
+        "score": 0.7,
+        "magnitude": 0.7,
+        "content": "Yo creo que es una idea fantástica y maravillosa, ¿A que sí?."
     }, {
         "score": 0.8,
         "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
+        "content": "Max es muy listo e inteligente, es el mejor amigo que puedes tener."
+    }, {
+        "score": 0.9,
+        "magnitude": 0.9,
+        "content": "Todos estamos felices y contentos, me encanta mi vida."
+    }, {
+        "score": 0.9,
+        "magnitude": 0.9,
+        "content": "La alegría de saber que este trabajo va a terminar perfecto me reconforta."
+    }, {
+        "score": 0.9,
+        "magnitude": 0.9,
+        "content": "Es muy gracioso, me lo pasó muy bien contigo."
+    }, {
+        "score": 0.7,
+        "magnitude": 0.7,
+        "content": "Pero el mejor punto de vista es estar alegres e increíblemente bien con uno mismo."
+    }, {
+        "score": 0.7,
+        "magnitude": 0.7,
+        "content": "Mientras todos sean felices esta bien, creo yo."
+    }, {
+        "score": 0.9,
+        "magnitude": 0.9,
+        "content": "En el mío hay un montón de flores y arcoíris por doquier, es el lugar más maravilloso y perfecto para entablar una conversación con las personas que más quieres en el mundo."
+    }, {
+        "score": 0.4,
+        "magnitude": 0.4,
+        "content": "Si vinieras aquí estarías más animado, te lo digo yo, aquí repartimos paz y mucho amor hacia el prójimo."
+    }, {
+        "score": 0.5,
+        "magnitude": 0.5,
+        "content": "Ya sabéis, chicos, a demostrar lo mucho que nos queremos en los mensajes."
     }, {
         "score": 0.8,
         "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
+        "content": "Chicos, tenéis que hablar más, demostrando lo mucho que nos amamos."
     }, {
         "score": 0.8,
         "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
-    }, {
-        "score": 0.8,
-        "magnitude": 0.8,
-        "content": "Yo te doy otra opción, ser feliz y vivir en armonía con los demás."
+        "content": "Dime en qué te puedo ayudar, me encanta ayudar a los demás."
     }, {
         "score": 0.8,
         "magnitude": 0.8,
@@ -63,61 +63,61 @@ let peopleData = [{
 }, {
     "score": 0.0,
     "sentences": [{
-        "score": -0.3,
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "Bipolar."
+    }, {
+        "score": -0.6,
+        "magnitude": 0.6,
+        "content": "Max no tiene ni idea de hablar inglés."
+    }, {
+        "score": -0.4,
+        "magnitude": 0.4,
+        "content": "Estoy harto de Raúl y de github."
+    }, {
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "Me quiero morir."
+    }, {
+        "score": -0.5,
+        "magnitude": 0.5,
+        "content": "No he podido escribir nada de código en toda la mañana.."
+    }, {
+        "score": 0.3,
         "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
+        "content": "Soy feliz."
+    }, {
+        "score": 0.5,
+        "magnitude": 0.5,
+        "content": "Raúl, me gustaría hacerte una recomendación desinteresada e informal: no vuelvas a tocar git en tu vida."
+    }, {
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Soy tan feliz ahora mismo que me arrancaría la cabeza."
+    }, {
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "Me gusta poder avanzar en los trabajos, pero no puedo, por lo que siento una enorme felicidad al imaginarme mi suicidio."
+    }, {
+        "score": 0.3,
+        "magnitude": 0.3,
+        "content": "Me voy a suicidar por lo maravillosa que es la vida."
+    }, {
+        "score": 0.9,
+        "magnitude": 0.9,
+        "content": "La alegría me embarga al pensar en los buenos amigos que tengo."
+    }, {
+        "score": -0.1,
+        "magnitude": 0.1,
+        "content": "Odio a mis compañeros de clase con toda mi alma."
+    }, {
+        "score": 0.9,
+        "magnitude": 0.9,
+        "content": "La amistad y la confianza son lo mejor de este mundo."
     }, {
         "score": -0.3,
         "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
-    }, {
-        "score": -0.3,
-        "magnitude": 0.3,
-        "content": "Max pierde el tiempo aqui en vez de ayudar en lo importante."
+        "content": "La naturaleza humana se basa en el odio y la traición."
     }, {
         "score": -0.3,
         "magnitude": 0.3,
@@ -129,121 +129,113 @@ let peopleData = [{
 }, {
     "score": 0.0,
     "sentences": [{
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.8,
+        "magnitude": 0.8,
+        "content": "It's a great day, you want to stay and we're going for a walk in the park?."
+    }, {
+        "score": 0.3,
+        "magnitude": 0.3,
+        "content": "You guys are always available."
+    }, {
+        "score": 0.7,
+        "magnitude": 0.7,
+        "content": "That's cool."
+    }, {
+        "score": 0.8,
+        "magnitude": 0.8,
+        "content": "Thanks for the compliment."
+    }, {
+        "score": 0.6,
+        "magnitude": 0.6,
+        "content": "so nice of you."
+    }, {
+        "score": 0.2,
+        "magnitude": 0.2,
+        "content": "I am currently working on the charts and data flow."
+    }, {
+        "score": 0.8,
+        "magnitude": 0.8,
+        "content": "I am happy that you are happy."
+    }, {
+        "score": -0.6,
+        "magnitude": 0.6,
+        "content": "I think you are not ok, I recommend you to go to the psychologist and think about what is worrying you and what is not."
+    }, {
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Now that I am thinking, should I go too?"
+    }, {
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Sometime I wonder why people have these kind of problems."
+    }, {
+        "score": -0.6,
+        "magnitude": 0.6,
+        "content": "Life is not easy at all."
+    }, {
+        "score": 0.6,
+        "magnitude": 0.6,
+        "content": "Hahaha, so funny."
     }, {
         "score": -0.1,
         "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "content": "I couple years ago I moved to Spain and in the last few years I have seen that political parties like Vox are raising popularity, so, Gema, what is your opinion about that?."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.2,
+        "magnitude": 0.2,
+        "content": "Well..."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": -0.2,
+        "magnitude": 0.2,
+        "content": "I don't think so...."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Right now?"
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "I don't have opinion on that."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "First I have to answer other questions about myself, why are we here?"
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "where are we going?"
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": -0.3,
+        "magnitude": 0.3,
+        "content": "Too much questions for too little time."
     }, {
-        "score": -0.1,
+        "score": 0.1,
         "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "content": "Right now, Barajas is the majority in this group."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.2,
+        "magnitude": 0.2,
+        "content": "Shut up."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": -0.2,
+        "magnitude": 0.2,
+        "content": "I don't get it."
     }, {
-        "score": -0.1,
+        "score": 0.1,
         "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "content": "We need more data, more messages, don't stop typing."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": -0.9,
+        "magnitude": 0.9,
+        "content": "This is really boring."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": -0.8,
+        "magnitude": 0.8,
+        "content": "I hate doing this all day."
     }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
-    }, {
-        "score": -0.1,
-        "magnitude": 0.1,
-        "content": "I will kill you maybe."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Please help me."
     }, {
         "score": -0.1,
         "magnitude": 0.1,
@@ -255,45 +247,45 @@ let peopleData = [{
 }, {
     "score": -0.4,
     "sentences": [{
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "score": -0.3,
+        "magnitude": 0.3,
+        "content": "Alex te odio, eres idiota."
+    }, {
+        "score": -0.8,
+        "magnitude": 0.8,
+        "content": "No funciona el maldito git."
+    }, {
+        "score": -0.1,
+        "magnitude": 0.1,
+        "content": "Eso es porque eres idiota."
+    }, {
+        "score": -0.5,
+        "magnitude": 0.5,
+        "content": "Te quiero ver muerto."
     }, {
         "score": -0.7,
         "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "content": "Mi opinión es que la vida es una mierda."
     }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Y que tu eres un desgraciado."
     }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "score": -0.4,
+        "magnitude": 0.4,
+        "content": "Max es un imbécil y además es de Barajas."
     }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "score": -0.5,
+        "magnitude": 0.5,
+        "content": "Eso no me lo dices en mi barrio, cobarde."
     }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "score": -0.9,
+        "magnitude": 0.9,
+        "content": "Es lo único interesante que tiene vuestro maldito pueblo porque el resto es una absoluta basura."
     }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
-    }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
-    }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
-    }, {
-        "score": -0.7,
-        "magnitude": 0.7,
-        "content": "Te doy 2 opciones: Acaba con mi vida o cierra la boca."
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "Cállate."
     }, {
         "score": -0.7,
         "magnitude": 0.7,
@@ -305,41 +297,41 @@ let peopleData = [{
 }, {
     "score": 0.3,
     "sentences": [{
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "pasivo."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.1,
+        "magnitude": 0.1,
+        "content": "Vale."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": -0.3,
+        "magnitude": 0.3,
+        "content": "Yo no tengo nada que hacer así que si queréis sí."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.8,
+        "magnitude": 0.8,
+        "content": "Pues yo estoy trabajando y no me puedo quejar de nada."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.7,
+        "magnitude": 0.7,
+        "content": "Todo depende del punto de vista."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.3,
+        "magnitude": 0.3,
+        "content": "Tiene sus cosas buenas y sus cosas malas."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.0,
+        "magnitude": 0.0,
+        "content": "Y cual es tú opinión?."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.8,
+        "magnitude": 0.8,
+        "content": "Lo podemos hablar en el barrio que quieras, pero en barajas se está bien."
     }, {
-        "score": 0.5,
-        "magnitude": 0.5,
-        "content": "Me parece correcto."
+        "score": 0.4,
+        "magnitude": 0.4,
+        "content": "En el mio hay un aeropuerto y con eso es suficiente."
     }, {
         "score": 0.5,
         "magnitude": 0.5,
